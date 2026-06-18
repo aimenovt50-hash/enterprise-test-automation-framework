636d1bbb5644507f654084b57ea4f7658b756d29
Timur Aimenov <aimenovt50@gmail.com>
2026-06-18 21:10:27 +0500

    fix: un-formatted error

--- src/data/factories/user_factory.py
+++ src/data/factories/user_factory.py
@@ -1,3 +1,3 @@
-from src.data.factories.base_factory import ProductFactory, UserFactory, UserData, ProductData
+from src.data.factories.base_factory import ProductData, ProductFactory, UserData, UserFactory
 
 __all__ = ["UserFactory", "ProductFactory", "UserData", "ProductData"]
